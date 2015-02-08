ed8e37f145a83368960b3796862dd9c5ad6196a0
Javier Lopez <javierlopm@hotmail.com>
2015-02-08 16:41:03 -0430

    Script bash para correr 10 pruebas de todos los tam

---
+++ correr.sh
@@ -0,0 +1,61 @@
+#! /bin/bash 
+make clean;
+make;
+
+RESULTADO=$PWD/salidaMari.txt;
+echo $RESULTADO;
+
+
+
+echo "Corriendo para archivo pruebaLongLong.txt"  > $RESULTADO;
+
+for y in "_p" "_t";
+do
+    for z in 5 10;
+    do
+        for x in {1..10};
+        do  
+            echo -n "prueba "$x" cript"$y" -c" $z" "  >> $RESULTADO;
+            ./cript$y -c $z pruebaLongLong.txt encriptado.txt >> $RESULTADO;
+            echo -n "         cript"$y" -d" $z" "  >> $RESULTADO;
+            ./cript$y -d $z encriptado.txt desencriptado.txt  >> $RESULTADO;
+            echo " " >> $RESULTADO;
+        done
+    done
+done
+
+for x in {1..10};
+    do  
+        echo $x >> $RESULTADO;
+        echo -n "   cript tam:" >> $RESULTADO;
+        ./cript -c pruebaLongLong.txt encriptado.txt >> $RESULTADO;
+        echo -n "   decript" >> $RESULTADO;
+        ./cript -d encriptado.txt desencriptado.txt  >> $RESULTADO;
+    done
+
+echo "Corriendo para archivo pruebaShort.txt"  >> $RESULTADO;
+
+for y in "_p" "_t";
+do
+    for z in 5 10;
+    do
+        for x in {1..10};
+        do  
+            echo -n "prueba "$x" cript"$y" -c" $z" "  >> $RESULTADO;
+            ./cript$y -c $z pruebaShort.txt encriptado.txt >> $RESULTADO;
+            echo -n "         cript"$y" -d" $z" "  >> $RESULTADO;
+            ./cript$y -d $z encriptado.txt desencriptado.txt  >> $RESULTADO;
+            echo " " >> $RESULTADO;
+        done
+    done
+done
+
+for x in {1..10};
+    do  
+        echo $x >> $RESULTADO;
+        echo -n "   cript tam:" >> $RESULTADO;
+        ./cript -c pruebaShort.txt encriptado.txt >> $RESULTADO;
+        echo -n "   decript" >> $RESULTADO;
+        ./cript -d encriptado.txt desencriptado.txt  >> $RESULTADO;
+    done
+
